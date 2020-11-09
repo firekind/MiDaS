7277df7812dcc78273cdb07c61a75afc3db64b7a
firekind <shyamant.achar@gmail.com>
2020-11-09 18:59:47 +0530

    added forward function that works with torch summary (somewhat)

--- midas/midas_net.py
+++ midas/midas_net.py
@@ -74,3 +74,32 @@ class MidasNet(BaseModel):
         out = self.scratch.output_conv(path_1)
 
         return layer_1, layer_2, layer_3, layer_4, torch.squeeze(out, dim=1)
+
+    def forward_for_summary(self, x):
+        """Forward pass for torch summary
+
+        Args:
+            x (tensor): input data (image)
+
+        Returns:
+            tensor: depth
+        """
+
+        layer_1 = self.pretrained.layer1(x)
+        layer_2 = self.pretrained.layer2(layer_1)
+        layer_3 = self.pretrained.layer3(layer_2)
+        layer_4 = self.pretrained.layer4(layer_3)
+
+        layer_1_rn = self.scratch.layer1_rn(layer_1)
+        layer_2_rn = self.scratch.layer2_rn(layer_2)
+        layer_3_rn = self.scratch.layer3_rn(layer_3)
+        layer_4_rn = self.scratch.layer4_rn(layer_4)
+
+        path_4 = self.scratch.refinenet4(layer_4_rn)
+        path_3 = self.scratch.refinenet3(path_4, layer_3_rn)
+        path_2 = self.scratch.refinenet2(path_3, layer_2_rn)
+        path_1 = self.scratch.refinenet1(path_2, layer_1_rn)
+
+        out = self.scratch.output_conv(path_1)
+
+        return layer_1, layer_2, layer_3, layer_4, torch.squeeze(out, dim=1)
